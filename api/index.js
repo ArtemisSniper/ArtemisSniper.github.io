6af3740ac7caba5a21c8dbc7dc1031ff72b42d4b
TheRedstoneRadiant <ibrahimeatspizza@gmail.com>
2022-03-26 23:09:34 -0400

    added vercel config

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
-const express = require("express");
-
-const app = express();
+const app = require("express")();
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.render("index");
 });
 
