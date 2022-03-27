aacbb3cbae54dd03bde54f601282903cd34a13af
Smint <ssg.jss2018@gmail.com>
2022-03-27 18:47:07 +0530

    [Update] Add Boilerplate Code for showing Discord Members and Stargazers on Website

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+// GET Request function for API
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+// Start Typer function
+function startTyping() {
+    var response = httpGet("")
+    var coolstuff = response.split(" ")
+    var discmem = coolstuff[1]
+    var stargzr = coolstuff[0]
+    new TypeIt("#DiscordMembers", {
+        speed: 50,
+    })
+      .type(discmem, { delay: 1200 })
+      .go()
+    new TypeIt("#Stargazers", {
+        speed: 50,
+    })
+      .type(stargzr, { delay: 1200 })
+      .go()
+}
