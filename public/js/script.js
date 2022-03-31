8739bc9f8d60098ea99ecd3a9da26ab0da22abd5
TheRedstoneRadiant <ibrahimeatspizza@gmail.com>
2022-03-31 12:29:58 -0400

    formatted code

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 // GET Request function for API
 function httpGet(url) {
-    let xhr  = new xhrRequest();
-    xhr.setRequestHeader("Access-Control-Allow-Origin","xhrRequest")
-    xhr.open( "GET", url, false );
-    xhr.send( null );
+    let xhr = new xhrRequest();
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "xhrRequest")
+    xhr.open("GET", url, false);
+    xhr.send(null);
     return xhr.responseText;
 }
 
@@ -15,20 +15,20 @@ function startTyping() {
     let discmem = coolstuff[1]
     let stargzr = coolstuff[0]
     new TypeIt("#DiscordMembers", {
-        speed: 50,
-        cursor: false,
-    })
-      .type(discmem, { delay: 1200 })
-      .go()
+            speed: 50,
+            cursor: false,
+        })
+        .type(discmem, { delay: 1200 })
+        .go()
     new TypeIt("#Stargazers", {
-        speed: 50,
-        cursor: false,
-    })
-      .type(stargzr, { delay: 1200 })
-      .go()
+            speed: 50,
+            cursor: false,
+        })
+        .type(stargzr, { delay: 1200 })
+        .go()
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-//    startTyping();
+document.addEventListener('DOMContentLoaded', function() {
+    //    startTyping();
     console.log("this works!")
 });
